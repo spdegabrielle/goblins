6886ab47d9cdcc96fe6f3d46b18eefa42c89ffed
Christopher Lemmer Webber <cwebber@dustycloud.org>
2018-10-22 15:16:30 -0400

    Add goblins dependencies

--- goblins/info.rkt
+++ goblins/info.rkt
@@ -1,7 +1,12 @@
 #lang info
 
 (define name "goblins")
-(define deps '())
+;; Some of these may just be development deps?
+(define deps '("base"))
+(define build-deps
+  '("scribble-lib"
+    "rackunit-lib"
+    "sandbox-lib"))
 (define pkg-desc "Actor library")
 (define version "0.0")
 (define pkg-authors '("cwebber"))
