2ae867287dab2259bb485d5ad818f55f7e766d08
Christopher Lemmer Webber <cwebber@dustycloud.org>
2019-06-04 14:56:53 -0400

    flesh out ref.rkt

--- ref.rkt
+++ ref.rkt
@@ -1,17 +1,26 @@
-#lang racket
+#lang racket/base
 
 (provide (struct-out near-ref)
          (struct-out far-ref)
          (struct-out remote-vat-ref))
 
+(require racket/match)
+
 (struct near-ref (debug-name)
-  #:transparent
-  #:constructor-name make-near-ref)
+  #:constructor-name make-near-ref
+  #:methods gen:custom-write
+  [(define (write-proc ref port mode)
+     (define str-to-write
+       (match (near-ref-debug-name ref)
+         [#f "#<near-ref>"]
+         ;; TODO: Do we need to do escaping?
+         [debug-name (format "#<near-ref ~a>" debug-name)]))
+     (write-string str-to-write port))])
 
 (struct far-ref (remote-vat-ref)
-  #:transparent
   #:constructor-name make-far-ref)
 
 ;; TODO: Do we add location hints here or somewhere else?
 ;;   Probably at the vat level and inter-vat level?
-(struct remote-vat-ref ())
+(struct remote-vat-ref ()
+  #:constructor-name make-remote-vat-ref)
