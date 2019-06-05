9d47e7b9bd1d99654473ffa0165e63010a5b750c
Christopher Lemmer Webber <cwebber@dustycloud.org>
2019-06-05 08:45:24 -0400

    derive near-ref and far-ref from ref

--- goblinoid/ref.rkt
+++ goblinoid/ref.rkt
@@ -1,12 +1,15 @@
 #lang racket/base
 
-(provide (struct-out near-ref)
+(provide ref?
+         (struct-out near-ref)
          (struct-out far-ref)
          (struct-out remote-vat-ref))
 
 (require racket/match)
 
-(struct near-ref (debug-name)
+(struct ref ())
+
+(struct near-ref ref (debug-name)
   #:constructor-name make-near-ref
   #:methods gen:custom-write
   [(define (write-proc ref port mode)
@@ -17,10 +20,11 @@
          [debug-name (format "#<near-ref ~a>" debug-name)]))
      (write-string str-to-write port))])
 
-(struct far-ref (remote-vat-ref)
+(struct far-ref ref (remote-vat-ref)
   #:constructor-name make-far-ref)
 
 ;; TODO: Do we add location hints here or somewhere else?
 ;;   Probably at the vat level and inter-vat level?
+;; NOTE: not derived from ref, maybe needs a new name?
 (struct remote-vat-ref ()
   #:constructor-name make-remote-vat-ref)
