79231b6684e54560bec7622e6730879c3bc85d3b
Christopher Lemmer Webber <cwebber@dustycloud.org>
2019-06-06 07:36:37 -0400

    Add perf-tests.rkt

---
+++ misc/perf-tests.rkt
@@ -0,0 +1,34 @@
+#lang racket
+
+(require goblinoid
+         racket/match)
+
+(define (do-actors-gc actormap)
+  (define (simple-actor)
+    'hello)
+  (time
+   (actormap-run!
+    actormap
+    (lambda ()
+      (for ([i 1000000])
+        (define friend
+          (spawn simple-actor))
+        (call friend))))))
+
+;; (collect-garbage 'major)
+
+(define (do-self-referential-actors-gc actormap)
+  (define (make-simple-actor)
+    (define self
+      (match-lambda*
+        [(list 'self) self]
+        [(list 'oop) 'boop]))
+    self)
+  (time
+   (actormap-run!
+    actormap
+    (lambda ()
+      (for ([i 1000000])
+        (define friend
+          (spawn (make-simple-actor)))
+        (call friend 'oop))))))
