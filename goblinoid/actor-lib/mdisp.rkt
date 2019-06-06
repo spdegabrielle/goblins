a441ae387e964033f7e33a97e27307ecf6c68d46
Christopher Lemmer Webber <cwebber@dustycloud.org>
2019-06-06 16:44:33 -0400

    Allow for some more options for describing method handlers

--- goblinoid/actor-lib/mdisp.rkt
+++ goblinoid/actor-lib/mdisp.rkt
@@ -8,22 +8,32 @@
 
 ;; Simple macro for method-based dispatching
 
-(define-syntax-rule (mdisp [(method-name method-args ...)
-                            body ...]
-                           ...)
-  (let ([methods
-         (make-hasheq
-          (list (cons (quote method-name)
-                      (lambda (method-args ...)
-                        body ...))
-                ...))])
-    (make-keyword-procedure
-     (lambda (kws kw-args method . args)
-       (define method-proc
-         (hash-ref methods method #f))
-       (unless method-proc
-         (error (format "No such method ~a" method)))
-       (keyword-apply method-proc kws kw-args args)))))
+(define-syntax do-method-defn
+  (syntax-rules ()
+    [(_ [(method-name method-args ...) body ...])
+     (cons (quote method-name)
+           (lambda (method-args ...)
+             body ...))]
+    [(_ [(method-name method-args ... . rest) body ...])
+     (cons (quote method-name)
+           (lambda (method-args ... . rest)
+             body ...))]
+    [(_ [method-name proc])
+     (cons (quote method-name) proc)]))
+
+(define-syntax mdisp
+  (syntax-rules ()
+    [(mdisp method-defn ...)
+     (let ([methods
+            (make-hasheq
+             (list (do-method-defn method-defn) ...))])
+       (make-keyword-procedure
+        (lambda (kws kw-args method . args)
+          (define method-proc
+            (hash-ref methods method #f))
+          (unless method-proc
+            (error (format "No such method ~a" method)))
+          (keyword-apply method-proc kws kw-args args))))]))
 
 (define-syntax-rule (define-mdisp id rest ...)
   (define id (mdisp rest ...)))
@@ -56,10 +66,14 @@
   (check-equal?
    (an-mdisp 'sing "george"
              "once upon a swingin' star..."
-             #:note-str "♫"
-             )
+             #:note-str "♫")
    "<george> ♫ once upon a swingin' star... ♫")
   (check-exn
    any/c
    (lambda ()
-     (an-mdisp 'nope))))
+     (an-mdisp 'nope)))
+
+  (check-equal?
+   ((mdisp [(foo . bar) bar])
+    'foo 'bar 'baz)
+   '(bar baz)))
