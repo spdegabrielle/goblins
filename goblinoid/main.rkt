7b1d42a7f2b3467d97a8b07febd9e3057c3d9755
Christopher Lemmer Webber <cwebber@dustycloud.org>
2019-06-13 12:58:07 -0400

    Just export the stuff that main.rkt does

--- goblinoid/main.rkt
+++ goblinoid/main.rkt
@@ -1,32 +1,6 @@
 #lang racket/base
 
-(provide make-actormap
-         snapshot-actormap hasheq->actormap
-         transactormap-merge!
-         actormap? transactormap? actormappable?
-
-         actormap-turn
-         actormap-poke!
-         actormap-peek
-         actormap-turn-message
-         actormap-run
-         actormap-run*
-         actormap-run!
-         actormap-spawn
-         actormap-spawn!
-
-         next
-
-         call spawn <-
-
-         (struct-out message)
-
-         near-ref? far-ref?
-
-         make-cell
-         spawn-cell
-         cell->read-only
-         cell->write-only)
+(provide (all-from-out "core.rkt"))
 
 (require "core.rkt"
          "message.rkt")
