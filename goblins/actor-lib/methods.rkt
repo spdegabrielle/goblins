0044472a24c3252338194443e9baab88b0e3c03b
Christopher Lemmer Webber <cwebber@dustycloud.org>
2019-10-24 12:27:15 -0400

    Add #:fallback option to methods, and tests

--- goblins/actor-lib/methods.rkt
+++ goblins/actor-lib/methods.rkt
@@ -6,11 +6,34 @@
          syntax/parse)
 
 (define-syntax (methods stx)
-  (define case-clauses
-    (match (syntax-e stx)
-      [(list _ method-defns ...)
-       (map (lambda (stx)
-              (syntax-parse stx
+  (define-values (case-clauses method-not-found-handler)
+    (let lp ([stx-to-process (cdr (syntax-e stx))]
+             [clauses '()]
+             [not-found-handler #f])
+      (match stx-to-process
+        ['()
+         (values (reverse clauses)
+                 (or not-found-handler
+                     #'(make-keyword-procedure
+                        (lambda (kws kw-args method . args)
+                          (error 'method-not-found "~a" method)))))]
+        [(list clause rest-clauses ...)
+         (cond
+           ;; Okay, we're setting up an fallback definition
+           [(eq? (syntax-e clause)
+                 '#:fallback)
+            (match rest-clauses
+              [(list not-found-handler rest-clauses ...)
+               (lp rest-clauses
+                   clauses
+                   not-found-handler)]
+              ['()
+               (raise-syntax-error
+                'methods-fallback-definition
+                "#:fallback must be followed by a procedure")])]
+           [else
+            (define new-clause
+              (syntax-parse clause
                 [((method-name:id method-args ...) body ...)
                  #'('method-name
                     (procedure-rename
@@ -25,7 +48,9 @@
                      'method-name))]
                 [(method-name:id proc)
                  #'('method-name proc)]))
-            method-defns)]))
+            (lp rest-clauses
+                (cons new-clause clauses)
+                not-found-handler)])])))
   
   #`(procedure-rename
      (make-keyword-procedure
@@ -33,11 +58,46 @@
         (define method-proc
           (case method
             #,@case-clauses
-            [else
-             (error 'method-not-found "~a" method)]))
-        (keyword-apply method-proc kws kw-args args)))
+            [else #f]))
+        (if method-proc
+            (keyword-apply method-proc kws kw-args args)
+            (#,method-not-found-handler method))))
      'methods))
 
 (module+ test
-  (require rackunit
-           "../core.rkt"))
+  (require rackunit)
+  (define some-methods
+    (methods
+     [(foo x)
+      (list 'foo x)]
+     [bar
+      (lambda (x)
+        (list 'bar x))]
+     [(baz . args)
+      (list 'baz args)]))
+  (check-equal?
+   (some-methods 'foo 'beep)
+   '(foo beep))
+  (check-equal?
+   (some-methods 'bar 'beep)
+   '(bar beep))
+  (check-equal?
+   (some-methods 'baz 'beep 'boop 'bop)
+   '(baz (beep boop bop)))
+
+  (define some-methods-with-custom-fallback
+    (methods
+     #:fallback
+     (make-keyword-procedure
+      (lambda _ 'haha-fallback))
+     [(foo x)
+      (list 'foo x)]
+     [bar
+      (lambda (x)
+        (list 'bar x))]
+     [(baz . args)
+      (list 'baz args)]))
+
+  (check-equal?
+   (some-methods-with-custom-fallback 'blorp)
+   'haha-fallback))
