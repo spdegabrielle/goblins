a48fef949ae8f82e62026d288d1960bfdc0438f4
Christopher Lemmer Webber <cwebber@dustycloud.org>
2020-06-07 23:25:57 -0400

    Add question-message

--- goblins/message.rkt
+++ goblins/message.rkt
@@ -1,6 +1,10 @@
 #lang racket/base
 
-(provide (struct-out message))
+(provide (struct-out message)
+         (struct-out question-message))
 
 (struct message (to resolve-me kws kw-vals args)
+  #:transparent)
+
+(struct question-message message (answer-this-question)
   #:transparent)
