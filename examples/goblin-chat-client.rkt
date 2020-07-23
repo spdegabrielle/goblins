e0fe5366083087e56542f1bac9372a34645f468b
Christopher Lemmer Webber <cwebber@dustycloud.org>
2020-07-23 17:05:14 -0400

    talk in chat microprogram over captp

--- examples/goblin-chat-client.rkt
+++ examples/goblin-chat-client.rkt
@@ -3,21 +3,38 @@
 (provide start-client)
 
 (require racket/unix-socket
+         racket/async-channel
          goblins/captp
-         pk
-         syrup)
+         goblins
+         pk)
 
-(define things-to-say
-  '((howdy partner)
-    (how go the "cowpokes" up on "farm" 33.2 "?")
-    (its mighty #"hot" down here)))
+
+(define ((^says-hello bcom))
+  "Hello from the client!")
 
 (define (start-client unix-domain-socket-path)
   (define-values (ip op)
     (unix-socket-connect unix-domain-socket-path))
   (pk 'client-connected 'ip ip 'op op)
-  (for ([sayin things-to-say])
-    (syrup-write sayin op))
-  (flush-output op)
-  (values ip op))
+  (define comms-vat
+    (make-vat))
+  (define client-vat
+    (make-vat))
+  (define bs-actor
+    (client-vat 'spawn ^says-hello))
+  (define machinetp-ch
+    (make-machinetp-thread ip op
+                           comms-vat
+                           bs-actor))
+  (define bootstrap-vow
+    (let ([return-ch (make-channel)])
+      (async-channel-put machinetp-ch
+                         (vector 'get-bootstrap-promise return-ch))
+      (channel-get return-ch)))
+
+  (client-vat 'run
+              (lambda ()
+                (on (<- bootstrap-vow)
+                    (lambda (val)
+                      (pk 'client-got-back val))))))
 
--- examples/goblin-chat-server.rkt
+++ examples/goblin-chat-server.rkt
@@ -6,21 +6,20 @@
          goblins
          goblins/captp
          goblins/actor-lib/methods
-         syrup
          pk)
 
-(define (handle-conn ip op)
-  (pk 'conn-thread)
-  (let lp ()
-    (call/ec
-     (lambda (return-early)
-       (match (syrup-read ip)
-         [(? eof-object?)
-          (pk 'client-disconnect)
-          (return-early)]
-         [msg
-          (pk 'client-sez msg)])
-       (lp)))))
+(define (handle-conn ip op bootstrap-actor)
+  ;; dunno if this is the right place I guess...
+  ;; really one per connection?  maybe excessive?
+  (define comms-vat
+    (make-vat))
+
+  (define machinetp-ch
+    (make-machinetp-thread ip op
+                           comms-vat
+                           bootstrap-actor))
+
+  'TODO-maybe-or-its-fine-dunno)
 
 (define (^chatroom bcom chatroom-name)
   (define-cell participants-by-name
@@ -43,17 +42,21 @@
 
   )
 
+(define ((^says-hello bcom))
+  "Hello from the server!")
+
 (define (run-server usock-listener)
-  (define comms-vat
-    (make-vat))
   (define chatroom-vat
     (make-vat))
+  (define chatroom
+    (chatroom-vat 'spawn ^says-hello))
+  (pk 'server-running)
   (let lp ()
     (sync (handle-evt (unix-socket-accept-evt usock-listener)
                       (match-lambda
                         [(list ip op)
                          (pk 'server-connected 'ip ip 'op op)
-                         (thread (lambda () (handle-conn ip op)))])))
+                         (handle-conn ip op chatroom)])))
     (lp)))
 
 (define (start-server unix-domain-socket-path)
